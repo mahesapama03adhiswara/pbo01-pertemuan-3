8747480c85afdc5ea3b1a8aa91c1e2debfe5cbcb
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-15 16:29:35 +0800

    switch menggnakan char, pilihan berbagai character

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,23 +2,29 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        int menuMakanan = 3;
-        switch (menuMakanan){
-            case 1 :
-                System.out.println("nasi goreng");
-                break;
-            case 2 :
-                System.out.println("mie goreng");
-                break;
-            case 3 :
-                System.out.println("mie kuah");
-                break;
-            case 4 :
-                System.out.println("mie ayam");
-                break;
-            case 5 :
-                System.out.println("bubur ayam");
-        }
+       char nilai ="B";
+        case 'A':
+        System.out.println("Mahasiswa :\"Terima kasih pak\"");
+        System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut \"");
+        break;
+        case'B':
+        System.out.println("Mahasiswa :\"pak apakah ada yang kurang dari hasil pembelajaran saya knp kd dapat nila A\"");
+        System.out.println("Dosen      :\"kamu ada ga masuk skali nak pas kelas bapak :) \"");
+        break;
+        case 'C':
+        System.out.println("Mahasiswa :\"ulun ngerjaakn pak aae tugas wn turun trus knp C nilai u\"");
+        System.out.println("Dosen      :\"semlam ujian mencotekk yang ssalah klo wkwkwkw  jd jwaabn bnyk salah:) \"");
+        break;
+        default:
+        System.out.println("Mahasiswa :\"alhamdulillah pak terimaksih nilai saya baik\"");
+        System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut sama kamu \"");
+        System.out.println("Mahasiswa :\"Terima kasih pak\"");
+        System.out.println("Dosen      :\"oh ya sampai ketemu di kelas bp lagi di semester 6 \"");
+        System.out.println("Mahasiswa :\"Terima kasih pak\"");
+        System.out.println("Dosen      :\"Ashiapppppppp.....:)\"");
+        break
+
+
 
     }
 }
