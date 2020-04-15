c55c22afb89974ac1b4b32f57cbf8575e044e436
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-15 15:10:03 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+    //opertaor (=) Sebagai Assigment / penuugasan
+    //menempatkan nilai kedalam variable
+    //baris 5 -7 adalah comment / komentar yang tidak dijakankan oleh program
+
+    String nama ="Mahesa Pama Adhiswara";
+    int jumlahSks = 24;
+    double indekPrestasiKumulatif =3.89;
+    
+}
