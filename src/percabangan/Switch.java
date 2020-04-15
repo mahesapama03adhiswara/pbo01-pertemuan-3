ca5e9922380a6645340d0cd3654d0c42fb9731e8
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-15 16:08:24 +0800

    pemilihan atau pergantian , objek

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int menuMakanan = 3;
+        switch (menuMakanan){
+            case 1 :
+                System.out.println("nasi goreng");
+                break;
+            case 2 :
+                System.out.println("mie goreng");
+                break;
+            case 3 :
+                System.out.println("mie kuah");
+                break;
+            case 4 :
+                System.out.println("mie ayam");
+                break;
+            case 5 :
+                System.out.println("bubur ayam");
+        }
+
+    }
+}
