57c2445c5fb576bbbf38dfcd037e881179c69db0
mahesapama03adhiswara <mahesapamaadhiswara@gmail.com>
2020-04-15 16:40:17 +0800

    switch menggnakan char, pilihan berbagai character

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,28 +2,29 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-       char nilai ="B";
+       char nilai ='B';
+       switch (nilai) {
         case 'A':
-        System.out.println("Mahasiswa :\"Terima kasih pak\"");
-        System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut \"");
-        break;
-        case'B':
-        System.out.println("Mahasiswa :\"pak apakah ada yang kurang dari hasil pembelajaran saya knp kd dapat nila A\"");
-        System.out.println("Dosen      :\"kamu ada ga masuk skali nak pas kelas bapak :) \"");
-        break;
+         System.out.println("Mahasiswa :\"Terima kasih pak\"");
+         System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut \"");
+         break;
+        case 'B':
+         System.out.println("Mahasiswa :\"pak apakah ada yang kurang dari hasil pembelajaran saya knp kd dapat nila A\"");
+         System.out.println("Dosen      :\"kamu ada ga masuk skali nak pas kelas bapak :) \"");
+         break;
         case 'C':
-        System.out.println("Mahasiswa :\"ulun ngerjaakn pak aae tugas wn turun trus knp C nilai u\"");
-        System.out.println("Dosen      :\"semlam ujian mencotekk yang ssalah klo wkwkwkw  jd jwaabn bnyk salah:) \"");
-        break;
+         System.out.println("Mahasiswa :\"ulun ngerjaakn pak aae tugas wn turun trus knp C nilai u\"");
+         System.out.println("Dosen      :\"semlam ujian mencotekk yang ssalah klo wkwkwkw  jd jwaabn bnyk salah:) \"");
+         break;
         default:
-        System.out.println("Mahasiswa :\"alhamdulillah pak terimaksih nilai saya baik\"");
-        System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut sama kamu \"");
-        System.out.println("Mahasiswa :\"Terima kasih pak\"");
-        System.out.println("Dosen      :\"oh ya sampai ketemu di kelas bp lagi di semester 6 \"");
-        System.out.println("Mahasiswa :\"Terima kasih pak\"");
-        System.out.println("Dosen      :\"Ashiapppppppp.....:)\"");
-        break
-
+         System.out.println("Mahasiswa :\"alhamdulillah pak terimaksih nilai saya baik\"");
+         System.out.println("Dosen      :\"selmat ya kamu dapat yang terbaik bapak salut sama kamu \"");
+         System.out.println("Mahasiswa :\"Terima kasih pak\"");
+         System.out.println("Dosen      :\"oh ya sampai ketemu di kelas bp lagi di semester 6 \"");
+         System.out.println("Mahasiswa :\"Terima kasih pak\"");
+         System.out.println("Dosen      :\"Ashiapppppppp.....:)\"");
+         break;
+       }
 
 
     }
